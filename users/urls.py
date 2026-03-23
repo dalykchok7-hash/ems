c67d7aac550ab9bc61b9e554d095bcea0b85d9fd
kchokdaly7@gmail.com <kchokmedali1@gmail.com>
2026-03-23 22:12:04 +0100

    changement 2

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from users.views import (
     LoginView,
-    CreerPersonnelView,
     LogoutView,
+    CreerPersonnelView,
     DashboardStatsView,
     PersonnelListView,
     PersonnelDetailView,
 )
 
 urlpatterns = [
-    path('login/',    LoginView.as_view()),
-    path('logout/',             LogoutView.as_view(),         name='logout'),
-    path('',          CreerPersonnelView.as_view()),
+    path('login/',     LoginView.as_view(),          name='login'),
+    path('logout/',    LogoutView.as_view(),          name='logout'),
+    path('',           CreerPersonnelView.as_view(),  name='creer-personnel'),
     path('dashboard/', DashboardStatsView.as_view(),  name='dashboard-stats'),
+    path('personnel/', PersonnelListView.as_view(),   name='personnel-list'),
+    path('personnel/<uuid:personnel_id>/',
+         PersonnelDetailView.as_view(),
+         name='personnel-detail'),
 ]
