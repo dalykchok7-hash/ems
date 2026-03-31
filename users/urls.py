639377a3aec51b87d5fc3d2f03c791dc65211c05
kchokdaly7@gmail.com <kchokmedali1@gmail.com>
2026-03-31 21:11:25 +0100

    fix dashb

--- users/urls.py
+++ users/urls.py
@@ -3,16 +3,20 @@ from users.views import (
     LoginView,
     LogoutView,
     CreerPersonnelView,
-    DashboardStatsView,
+    DashboardRevenusView,
     PersonnelListView,
     PersonnelDetailView,
+    DashboardAlertesView,
+    DashboardClientsView,
 )
 
 urlpatterns = [
     path('login/',     LoginView.as_view(),          name='login'),
     path('logout/',    LogoutView.as_view(),          name='logout'),
     path('',           CreerPersonnelView.as_view(),  name='creer-personnel'),
-    path('dashboard/', DashboardStatsView.as_view(),  name='dashboard-stats'),
+    path('dashboard/revenus/', DashboardRevenusView.as_view(), name='dashboard-revenus'),
+    path('dashboard/alertes/', DashboardAlertesView.as_view(), name='dashboard-alertes'),
+    path('dashboard/clients/', DashboardClientsView.as_view(), name='dashboard-clients'),
     path('personnel/', PersonnelListView.as_view(),   name='personnel-list'),
     path('personnel/<uuid:personnel_id>/',
          PersonnelDetailView.as_view(),
