4b3bb2be578625363d84034103186382c641b658
kchokdaly7@gmail.com <kchokmedali1@gmail.com>
2026-04-05 02:17:39 +0100

    fix email unique 2

--- users/serializers/utilisateur_serializer.py
+++ users/serializers/utilisateur_serializer.py
@@ -30,7 +30,7 @@ class CreerPersonnelSerializer(serializers.Serializer):
 
     username      = serializers.CharField(max_length=150)
     password      = serializers.CharField(max_length=128, write_only=True)
-    email         = serializers.EmailField(required=False, default='')  # ← ajouter
+    email         = serializers.EmailField(required=True)
     first_name    = serializers.CharField(max_length=100)
     last_name     = serializers.CharField(max_length=100)
     cin           = serializers.CharField(max_length=20)
@@ -38,11 +38,21 @@ class CreerPersonnelSerializer(serializers.Serializer):
     shift         = serializers.ChoiceField(choices=['jour', 'soir'])
     date_embauche = serializers.DateField()
 
+    def validate_email(self, value):
+        if Utilisateur.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("Cet email est déjà utilisé.")
+        return value
+
+    def validate_username(self, value):
+        if Utilisateur.objects.filter(username__iexact=value).exists():
+            raise serializers.ValidationError("Ce nom d'utilisateur est déjà pris.")
+        return value
+
     def validate_cin(self, value):
         if len(value) != 8:
-            raise serializers.ValidationError(
-                "Le CIN doit contenir 8 chiffres"
-            )
+            raise serializers.ValidationError("Le CIN doit contenir 8 chiffres.")
+        if Utilisateur.objects.filter(cin__iexact=value).exists():
+            raise serializers.ValidationError("Ce CIN est déjà utilisé.")
         return value
 
 
